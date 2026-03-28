c9659edd948d2bf01a0f4fdd54426e0e9a968b9d
R216YU <r216yu@gmail.com>
2026-03-29 01:07:59 +0900

    update: post-repository更新

--- src/shared/repository/post-repository.ts
+++ src/shared/repository/post-repository.ts
@@ -1,5 +1,5 @@
-class PostRepository {}
-
-const postRepository = new PostRepository();
-
-export { postRepository };
+export const getPosts = async () => {};
+export const getPostById = async (id: number) => {};
+export const createPost = async () => {};
+export const updatePost = async () => {};
+export const deletePost = async () => {};
